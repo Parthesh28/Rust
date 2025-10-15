e9fccb8af2d4b32ab98e8828860042bcbb11e5fe
Parthesh28 <partheshpurohit28@gmail.com>
2025-10-15 20:31:55 +0530

    refactored code

--- multithreading/src/main.rs
+++ multithreading/src/main.rs
@@ -1,5 +1,5 @@
 use::std::net::TcpListener;
-use std::{fmt::format, fs, io::{BufRead, BufReader, Write}, net::TcpStream};
+use std::{ fs, io::{BufRead, BufReader, Write}, net::TcpStream};
 
 fn main(){
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,23 +16,18 @@ fn main(){
         let buf_reader = BufReader::new(&stream);
         let req_line = buf_reader.lines().next().unwrap().unwrap();
 
-      if req_line == "GET / HTTP/1.1"{
-          let status_code = "HTTP/1.1 200 OK";
-          let body = fs::read_to_string("src/index.html").unwrap();
-          let length = body.len();
-          
-          let res = format!("{status_code}\r\nContent-Length: {length}\r\n\r\n{body}");
-          
-          stream.write_all(res.as_bytes()).unwrap();
-        } else{
-            let status_code = "HTTP/1.1 404 NOT FOUND";
-            let body = fs::read_to_string("src/error.html").unwrap();
-            let length = body.len();
-            
-            let res = format!("{status_code}\r\nContent-Length: {length}\r\n\r\n{body}");
-            stream.write_all(res.as_bytes()).unwrap();
+        let (status_code, filename) = if req_line == "GET / HTTP/1.1"{ ("HTTP/1.1 200 OK", "src/index.html")}
+         else {("HTTP/1.1 404 NOT FOUND", "src/error.html")};
+         
 
-      }  
+
+        let body  = fs::read_to_string(filename).unwrap();
+
+        let length = body.len();
+
+        let resp =  format!("{status_code}\r\nContent-Length: {length}\r\n\r\n{body}");
+
+        stream.write_all(resp.as_bytes()).unwrap();
 
     }
 }
