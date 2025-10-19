db7ee87ef48f8accda7483100030a66a82caa541
Parthesh28 <partheshpurohit28@gmail.com>
2025-10-19 12:24:09 +0530

    improved io project

--- input_output/src/lib.rs
+++ input_output/src/lib.rs
@@ -1,34 +1,17 @@
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-     let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> impl Iterator<Item = &'a str> {
+    contents.lines().filter(move |line| line.contains(query))
 }
 
 pub fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str,
-) -> Vec<&'a str> {
+) -> impl Iterator<Item = &'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(move |line| line.to_lowercase().contains(&query))
 }
-
-
-
-// --snip--
 
 #[cfg(test)]
 mod tests {
@@ -43,7 +26,10 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents).collect::<Vec<_>>()
+        );
     }
 
     #[test]
@@ -56,8 +42,8 @@ Pick three.
 Trust me.";
 
         assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents).collect::<Vec<_>>()
         );
     }
 }
--- multithreading/src/lib.rs
+++ multithreading/src/lib.rs
@@ -1,14 +1,47 @@
-pub struct ThreadPool;
+use std::{sync::mpsc::{self}, thread};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Job;
+
 
 impl ThreadPool {
+
     pub fn new(size: usize) -> ThreadPool {
-        assert!(size >0);
-        ThreadPool
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver));
+        }
+
+        ThreadPool { workers , sender}
     }
 
-     pub fn execute<F>(&self, f: F)
+    pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: mpsc::Receiver<Job> ) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+
+        Worker { id, thread }
     }
 }
